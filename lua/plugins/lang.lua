ed636ced1a12824c5c1e72cc099f3935765e58ec
rea <hi@rea.ink>
2023-05-11 18:22:06 +0800

    fix: add java,rust,nodejs lang, dap

---
+++ lua/plugins/lang.lua
@@ -0,0 +1,8 @@
+return {
+  { import = "lazyvim.plugins.extras.dap.nlua" },
+
+  -- custom language specific extension modules
+  { import = "plugins.extras.lang.java" },
+  { import = "plugins.extras.lang.rust" },
+  { import = "plugins.extras.lang.nodejs" },
+}
